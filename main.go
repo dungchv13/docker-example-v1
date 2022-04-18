7cc2106e9a463fe9331feb61a2b929d22ed896ff
dungchv13 <dungutc13@gmail.com>
2022-04-18 09:53:59 +0700

    abc

--- main.go
+++ main.go
@@ -3,18 +3,90 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	handler := http.HandlerFunc(handleRequest)
-	http.Handle("/photo", handler)
+	handler := http.HandlerFunc(getList)
+
+	http.Handle("/", handler)
 	http.ListenAndServe(":8080", nil)
+}
+
+type user struct {
+	ID   primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name string             `json:"name,omitempty" bson:"name,omitempty"`
+	Age  int                `json:"age,omitempty" bson:"age,omitempty"`
+}
+
+func getConnection() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://admin:password@localhost:27017/"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	return client
+
+}
+
+func getList(w http.ResponseWriter, r *http.Request) {
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://admin:password@mongodb:27017/"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	collection := client.Database("my-db").Collection("users")
+	cur, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(ctx)
+	var users []user
+	for cur.Next(ctx) {
+		var result user
+		err := cur.Decode(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+		users = append(users, result)
+
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("len : ", len(users))
+	json.NewEncoder(w).Encode(users)
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
